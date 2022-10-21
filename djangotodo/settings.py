11ea2f368585ff808fa02e22018ce8dee00cd4f5
adam chen <tmac19920626@gmail.com>
2022-10-21 20:03:41 +0800

    1021

--- djangotodo/settings.py
+++ djangotodo/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
 
-    'herrmannbily.herokuapp.com','127.0.0.1','btktech.com.tw','wwww.btktech.com.tw'
+    'btktechcom-production.up.railway.app','127.0.0.1','btktech.com.tw','wwww.btktech.com.tw'
 
 ]
 
