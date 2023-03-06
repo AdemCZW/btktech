3d250abd306bc1374d70b48d5370461dbfe0f61b
adam chen <tmac19920626@gmail.com>
2023-03-06 16:22:29 +0800

    0306

--- polyacetal/serializers.py
+++ polyacetal/serializers.py
@@ -6,4 +6,5 @@ from .models import home_poly
 class poly_serializer(ModelSerializer):
     class Meta:
         model = home_poly
-        fields = ['id', 'poly_index_001']
+        fields = ['id', 'poly_index_001', 'poly_index_002', 'poly_index_003',
+                  'poly_index_004', 'poly_index_005', 'poly_index_006']
