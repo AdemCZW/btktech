ef24377f7dcf8b0038c78a00afc3654ed384bbd5
adam chen <tmac19920626@gmail.com>
2023-03-08 17:47:29 +0800

    333

--- polyacetal/urls.py
+++ polyacetal/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from rest_framework import routers
+from django.views.generic import TemplateView
 from .views import (
     poly_index,
     apiviewset,
@@ -16,4 +17,5 @@ urlpatterns = [
     path('api', views.poly_home, name='index-api'),
     path('api001', ListCreateAPIView.as_view(), name='list001'),
     path('del/<str:pk>', DetailAPIview.as_view(), name='del-api'),
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
